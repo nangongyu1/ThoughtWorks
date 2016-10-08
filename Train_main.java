16cc58a72e6859d88755e7a4f5b989d48b128b8f
nangongyu1 <yu.xiao.h@163.com>
2016-10-08 16:53:54 +0800

    Create Train_main.java
    程序入口

---
+++ Train_main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by yu on 2016/9/30.
+ * 方法入口
+ */
+public class Train_main {
+    public static void main(String[] args) {
+        ConsoleTrain consoleTrain = new ConsoleTrain();
+        System.out.println("Please input the Graphy:");
+        consoleTrain.dealWithInandOutput();
+        //AB5,BC4,CD8,DC8,DE6,AD5,CE2,EB3,AE7
+    }
+}
